dd9da78e6e1d14f2eed9bcd32da8261e97505fc1
진희헌 <ucb1122@naver.com>
2026-04-02 22:51:22 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+    
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        
+        System.out.println(first + second);
+    }
+}
