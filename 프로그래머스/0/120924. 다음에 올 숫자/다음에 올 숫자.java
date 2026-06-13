766dd2b663b60e415ecf6a9ce6e597d784b1b95a
진희헌 <ucb1122@naver.com>
2026-06-13 10:09:08 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        int len = common.length;
+        
+        if ((common[1] - common[0]) == (common[2] - common[1])) {
+            int d = common[1] - common[0];
+            answer = common[len - 1] + d;
+        } else {
+            int r = common[1] / common[0];
+            answer = common[len - 1] * r;
+        }
+        return answer;
+    }
+}
