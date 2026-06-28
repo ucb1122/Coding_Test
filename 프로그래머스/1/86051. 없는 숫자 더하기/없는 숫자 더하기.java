9818383e35af8c7c17ee0bd1e29de79ad6a9d400
진희헌 <ucb1122@naver.com>
2026-06-28 23:46:29 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i ++) {
+            sum += numbers[i];
+        }  
+        return 45 - sum; 
+    }
+}
