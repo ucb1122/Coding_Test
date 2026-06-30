6fd3c282d797068788feb80557a3bd580a41b676
진희헌 <ucb1122@naver.com>
2026-06-30 20:14:46 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,7 @@
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+  FROM DEVELOPERS D
+ WHERE EXISTS (SELECT 1 
+                 FROM SKILLCODES S
+                WHERE S.NAME IN ('Python', 'C#')
+                  AND (D.SKILL_CODE & S.CODE) > 0)
+ ORDER BY ID ASC;
