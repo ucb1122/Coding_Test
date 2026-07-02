f684e476bac90f03857a9d661866abfc5467c2d3
진희헌 <ucb1122@naver.com>
2026-07-02 14:43:51 +0900

    [level 1] Title: 기사단원의 무기, Time: 42.37 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,25 +1,57 @@
-import java.util.HashMap; 
+// import java.util.HashMap; 
+
+// class Solution {
+//     public int solution(int number, int limit, int power) {
+//         int answer = 0;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for (int i = 1; i <= number; i++) {
+//             map.put(i, countFactor(i, limit, power));
+//             answer += map.get(i);
+//         } 
+//         return answer;
+//     }
+    
+//     public int countFactor(int number, int limit, int power) {
+//         int count = 0;
+//         for (int i = 1; i <= number; i++) {
+//             if(number % i == 0) {
+//                 count++;
+//                 if(count > limit) {
+//                     count = power;
+//                     break;
+//                 }    
+//             }
+//         }
+//         return count; 
+//     }
+// }
 
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        
         for (int i = 1; i <= number; i++) {
-            map.put(i, countFactor(i, limit, power));
-            answer += map.get(i);
+            int count = countFactor(i);
+            
+            if (count > limit) {
+                answer += power;
+            } else {
+                answer += count;
+            }
         } 
         return answer;
     }
     
-    public int countFactor(int number, int limit, int power) {
+    public int countFactor(int n) {
         int count = 0;
-        for (int i = 1; i <= number; i++) {
-            if(number % i == 0) {
-                count++;
-                if(count > limit) {
-                    count = power;
-                    break;
-                }    
+        
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                if (i * i == n) {
+                    count++;
+                } else {
+                    count += 2;
+                }
             }
         }
         return count; 
