2f3de643a475a003c0f9c923ab2ac04b5dd0d6d0
진희헌 <ucb1122@naver.com>
2026-09-02 00:07:51 +0900

    [level 1] Title: [1차] 비밀지도, Time: 1.50 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,32 +1,19 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        int[][] result1 = new int[n][n];
-        int[][] result2 = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            int num1 = arr1[i];
-            int num2 = arr2[i];
-
-            for (int j = n - 1; j >= 0; j--) {
-                result1[i][j] = num1 % 2;
-                num1 = num1 / 2;
-
-                result2[i][j] = num2 % 2;
-                num2 = num2 / 2;
-            }
-        }
         String[] answer = new String[n];
 
         for (int i = 0; i < n; i++) {
-            String answerStr = "";
-            for (int j = 0; j < n; j++) {
-                if (result1[i][j] == 1 || result2[i][j] == 1) {
-                    answerStr += "#";
-                } else {
-                    answerStr += " ";
-                }
+            int combined = arr1[i] | arr2[i];
+
+            String binary = Integer.toBinaryString(combined);
+
+            while (binary.length() < n) {
+                binary = "0" + binary;
             }
-            answer[i] = answerStr;
+
+            binary = binary.replace('1', '#').replace('0', ' ');
+
+            answer[i] = binary;
         }
         return answer;
     }
