a0d4132fb669acd753f089299edf4dd6732703a3
진희헌 <ucb1122@naver.com>
2026-09-06 15:25:32 +0900

    [level 3] Title: 네트워크, Time: 0.15 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,24 @@
 class Solution {
     public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
         int answer = 0;
 
         for (int i = 0; i < n; i++) {
-            if (computers[i][i] == 0) continue;
-
-            answer++;
-
-            boolean changed = true;
-            while (changed) {
-                changed = false;
-                for (int j = 0; j < n; j++) {
-                    if (computers[j][j] == 1) {
-                        for (int k = 0; k < n; k++) {
-                            if (computers[j][k] == 1 && (computers[k][k] == 0 || j == i)) {
-                                computers[j][j] = 0;
-                                computers[k][k] = 0;
-                                changed = true;
-                            }
-                        }
-                    }
-                }
+            if (!visited[i]) {
+                dfs(i, n, computers, visited);
+                answer++;
             }
         }
+
         return answer;
+    }
+
+    private void dfs(int cur, int n, int[][] computers, boolean[] visited) {
+        visited[cur] = true;
+        for (int next = 0; next < n; next++) {
+            if (computers[cur][next] == 1 && !visited[next]) {
+                dfs(next, n, computers, visited);
+            }
+        }
     }
 }
